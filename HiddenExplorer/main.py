cdd3d163f47e8433dfdd89acd2b8d648dcd0be41
mino-38 <89204436+mino-38@users.noreply.github.com>
2022-06-23 21:24:08 +0900

    Update main.py

--- HiddenExplorer/main.py
+++ HiddenExplorer/main.py
@@ -1,9 +1,12 @@
 import glob
 import os
 import tempfile
-import tarfile
+import zipfile
 
 import wx
+
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES
 
 from HiddenExplorer.lib.crypto import encrypto, decrypto
 
@@ -13,29 +16,46 @@ root = os.path.join(os.environ.get("USERPROFILE"), ".HiddenExplorer")
 if not os.path.isdir(root):
     os.mkdir(root)
 crypto_file = os.path.join(root, ".data")
+key_file = os.path.join(root, ".key")
+
+def encrypt(key):
+    cipher = AES.new(key, AES.MODE_EAX)
+    with open(crypto_file, "wb+") as f:
+        f.write(cipher.encrypt(f.read()))
+
+def decrypt(key):
+    with open(crypto_file, "rb") as f:
+        data = f.read()
+    with open(key_file, "rb") as k:
+        cipher1 = AES.new(k.read(), AES.MODE_EAX)
+        data = cipher1.decrypt(data)
+    cipher2 = AES.new(key, AES.MODE_EAX)
+    with open(crypto_file, "rb") as f:
+        return cipher2.decrypt(data)
 
 class MainFrame(wx.Frame):
     size = (800, 500)
-    def __init__(self, bytes_):
+    def __init__(self, bytes_=None, files=None):
         super().__init__(None, title=TITLE, size=MainFrame.size)
         self.bytes = bytes_
+        self.files = files
         self.build()
 
     def build(self):
-        self.sizer = wx.BoxSizer()
-        self.psizer = wx.GridSizer(cols=4)
+        if hasattr(self, "sizer"):
+            self.sizer.Clear(True)
+        else:
+            self.sizer = wx.BoxSizer()
         self.panel = wx.Panel(self)
-        with NamedTemporaryFile("wb") as t, tempfile.TemporaryDirectory() as d:
-            t.write(self.bytes)
-            with tarfile.open(t.name, "r") as g:
-                g.extractall(d)
-                updater = wx.ProgressDialog(None, TITLE, "ロード中...", style=wx.PD_REMAINING_TIME | wx.PD_ELAPSED_TIME)
-                updater.Show()
-                for i in glob.iglob(os.path.join(d, "**"), recursive=True):
-                    self.set_layout(i)
-                updater.Destroy()
-        self.panel.SetSizer(self.psizer)
-        self.sizer.Add(self.panel)
+        if self.files:
+            self.psizer = wx.GridSizer(cols=4)
+            for p in self.files:
+                self.set_layout(p)
+            self.panel.SetSizer(self.psizer)
+            self.sizer.Add(self.panel)
+        else:
+            self.sizer.Add(wx.StaticText(self, wx.ID_ANY, "まだ何もありません"))
+            self.sizer.Add(self.panel)
         self.SetSizer(self.sizer)
 
     def set_layout(self, path):
@@ -45,10 +65,39 @@ class MainFrame(wx.Frame):
         panel.SetSizer(sizer)
         self.psizer.Add(panel)
 
+class AskPasswordFrame(wx.Frame):
+    size = (300, 200)
+    def __init__(self):
+        super().__init__(None, title=TITLE, size=AskPasswordFrame.size)
+        self.build()
+
+    def build(self):
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.panel = wx.Panel(self)
+        self.sizer.Add(wx.StaticText(self.panel, wx.ID_ANY, "HiddenExplorerを利用するパスワードを入力してください"))
+        self.ctrl = wx.TextCtrl(self.panel, size=(200, 50))
+        self.sizer.Add(self.ctrl)
+        self.error = wx.StaticText(self.panel)
+        self.sizer.Add(self.error)
+        self.button = wx.Button(self.panel, wx.ID_ANY, "決定")
+        self.button.Bind(wx.EVT_BUTTON, self.login)
+        self.sizer.Add(self.panel)
+        self.SetSizer(self.sizer)
+
+    def login(self):
+        password = self.ctrl.GetValue()
+        bytes_ = decrypt(password)
+        with tempfile.NamedTemporaryFile("wb") as f:
+            f.write(bytes_)
+            f.flush()
+            if zipfile.is_zipfile(f.name):
+                MainFrame(bytes_, zipfile.ZipFile(f.name).namelist())
+
 class InitFrame(wx.Frame):
     size = (500, 300)
-    def __init__(self):
+    def __init__(self, files):
         super().__init__(None, title=TITLE+"  初期化", size=InitFrame.size)
+        self.files = files
         self.build()
 
     def build(self):
@@ -70,9 +119,15 @@ class InitFrame(wx.Frame):
     def set_password(self, e):
         password = self.ctrl1.GetValue()
         if password == self.ctrl2.GetValue():
-            with open(crypto_file, "w") as f:
-                f.write("")
-            MainFrame(password).Show()
+            with tempfile.TemporaryDirectory() as d:
+                for p in self.files:
+                    shutil.move(p, d)
+                shutil.make_archive(crypto_file, "zip", d)
+                shutil.move(crypto_file+".zip", crypto_file)
+            key = get_random_bytes(AES.block_size*2)
+            encrypt(key)
+            with open(key_file, "wb") as f:
+                f.write(key)
             self.Destroy()
         else:
             self.error.SetLabel("パスワードが一致していません")
