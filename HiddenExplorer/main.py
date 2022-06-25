b52dccdcb58e73e2c6acc26e7c9db338c4ad71f9
mino-38 <89204436+mino-38@users.noreply.github.com>
2022-06-25 12:55:22 +0900

    Update main.py

--- HiddenExplorer/main.py
+++ HiddenExplorer/main.py
@@ -201,8 +201,7 @@ class MainFrame(wx.Frame):
             with open(temp_zip, "wb") as f:
                 f.write(self.bytes)
             with zipfile.ZipFile(temp_zip, "r") as z, tempfile.TemporaryDirectory() as d:
-                file = os.path.join(d, path)
-                z.extract(path, file)
+                file = z.extract(path, d)
             subprocess.run(["call", file], shell=True)
         finally:
             os.remove(temp_zip)
