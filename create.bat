d0ed864ef8316b24ca8eace8c3ed8b459d6f5927
mino-38 <89204436+mino-38@users.noreply.github.com>
2022-06-25 19:20:20 +0900

    Update create.bat

--- create.bat
+++ create.bat
@@ -1,4 +1,4 @@
-pip install --user -U pip setuptools nuitka zstandard
+pip install --user -U pip setuptools nuitka zstandard ordered-set
 pip install --user -U -r requirements.txt
 
 nuitka --onefile --windows-disable-console --standalone --mingw64 --follow-imports --include-data-dir=./HiddenExplorer/resources=resources --windows-icon-from-ico=./HiddenExplorer/resources/HiddenExplorer.ico -o HiddenExplorer.exe ./HiddenExplorer/main.py
