dec01921da2626664a5e6fc55d4fd4e51f0e4d29
mino-38 <89204436+mino-38@users.noreply.github.com>
2022-06-25 16:25:37 +0900

    Update main.py

--- HiddenExplorer/main.py
+++ HiddenExplorer/main.py
@@ -204,7 +204,7 @@ class MainFrame(wx.Frame):
             os.remove(temp_zip)
 
     def set_layout(self, path):
-        path = re.sub(r"\\+", r"\", path)
+        path = re.sub("\\\\+", "\\", path)
         sizer = wx.BoxSizer(wx.VERTICAL)
         panel = wx.Panel(self.panel, size=(150, 120))
         temp_zip = os.path.join(tempfile.gettempdir(), ".random_{}.{}".format(os.getpid(), time.time()))
