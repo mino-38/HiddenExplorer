77205e92b782fe0f604493cf33c8e63370056ffd
mino-38 <89204436+mino-38@users.noreply.github.com>
2022-06-25 18:57:41 +0900

    Update and rename build.bat to create.bat

--- build.bat
+++
@@ -1,4 +0,0 @@
-pip install --user -U pip setuptools nuitka zstandard
-pip install --user -U -r requirements.txt
-
-nuitka --onefile --standalone --mingw64 --follow-imports --include-data-dir=./HiddenExplorer/resources=resources --windows-icon-from-ico=./HiddenExplorer/resources/HiddenExplorer.ico -o HiddenExplorer.exe ./HiddenExplorer/main.py
