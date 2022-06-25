d02aa427e48da350d5225767986d0bcf50cf5de4
mino-38 <89204436+mino-38@users.noreply.github.com>
2022-06-25 21:49:57 +0900

    Update create.bat

--- create.bat
+++ create.bat
@@ -6,7 +6,9 @@ call ./compile/Scripts/activate
 
 pip install -U -r requirements.txt
 
-pyinstaller --clean --onefile --noconsole --add-data "./HiddenExplorer/resources;resources" --icon=./HiddenExplorer/resources/HiddenExplorer.ico -n HiddenExplorer.exe ./HiddenExplorer/main.py
+rem pyinstaller --clean --onefile --noconsole --add-data "./HiddenExplorer/resources;resources" --icon=./HiddenExplorer/resources/HiddenExplorer.ico -n HiddenExplorer.exe ./HiddenExplorer/main.py
+
+python -m nuitka  --mingw64 --windows-disable-console --follow-imports --standalone --onefile --include-data-dir=./HiddenExplorer/resources=resources --windows-icon-from-ico=./HiddenExplorer/resources/HiddenExplorer.ico -o HiddenExplorer.exe ./HiddenExplorer/main.py
 
 deactivate
 
