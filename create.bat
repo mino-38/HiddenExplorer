8bc9c58da745d4fc3adde2a5754dca4008e28ed3
mino-38 <89204436+mino-38@users.noreply.github.com>
2022-06-26 09:24:58 +0900

    Update create.bat

--- create.bat
+++ create.bat
@@ -1,15 +1,5 @@
-pip install --user -U pip setuptools virtualenv nuitka zstandard ordered-set Cython
-
-rem virtualenv compile
-
-rem call ./compile/Scripts/activate
+pip install --user -U pip setuptools nuitka zstandard ordered-set Cython
 
 pip install --user -U -r requirements.txt
 
-rem pyinstaller --clean --onefile --noconsole --add-data "./HiddenExplorer/resources;resources" --icon=./HiddenExplorer/resources/HiddenExplorer.ico -n HiddenExplorer.exe ./HiddenExplorer/main.py
-
 python -m nuitka  --mingw64 --windows-disable-console --follow-imports --standalone --onefile --include-data-dir=./HiddenExplorer/resources=resources --windows-icon-from-ico=./HiddenExplorer/resources/HiddenExplorer.ico -o HiddenExplorer.exe ./HiddenExplorer/main.py
-
-rem deactivate
-
-rem del /Q ./compile
