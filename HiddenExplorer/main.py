cacc8d0b375b1a522629f5a0bf7ab25675abe646
mino-38 <89204436+mino-38@users.noreply.github.com>
2022-07-03 12:54:47 +0900

    Update main.py

--- HiddenExplorer/main.py
+++ HiddenExplorer/main.py
@@ -329,6 +329,7 @@ class MainFrame(wx.Frame):
                         file = z.extract(path, os.path.join(d, os.path.splitext(path)[1]))
                     except:
                         file = z.extract(path+"/", os.path.join(d, path))
+                isdir = os.path.isdir(isdir)
                 try:
                     img = get_icon(file).resize((90, 100))
                     image = wx.Image(img.size[0], img.size[1])
@@ -336,19 +337,29 @@ class MainFrame(wx.Frame):
                     bmp = wx.StaticBitmap(panel, wx.ID_ANY, image.ConvertToBitmap())
                 except:
                     bmp = wx.StaticBitmap(panel, wx.ID_ANY, self.default_fileicon if os.path.isfile(file) else self.default_diricon)
-                bmp.Bind(wx.EVT_LEFT_DCLICK, RunFunction(self.run_file, path))
-                bmp.Bind(wx.EVT_RIGHT_UP, RunFunction(self.show_menu, path, os.path.isdir(file)))
                 sizer.Add(bmp, proportion=1)
         finally:
             if not zip:
                 os.remove(temp_zip)
         sizer.Add(wx.StaticText(panel, wx.ID_ANY, textwrap(path, 15)), proportion=1)
+        transpanel = wx.Panel(panel, size=panel.Size)
+        transpanel.Bind(wx.EVT_LEFT_DCLICK, RunFunction(self.run_file, path))
+        transpanel.Bind(wx.EVT_RIGHT_UP, RunFunction(self.show_menu, path, isdir))
+        transpanel.Bind(wx.EVT_ENTER_WINDOW, RunFunction(self.set_trans_color, transpanel, "#444444"))
+        transpanel.Bind(wx.EVT_LEAVE_WINDOW, RunFunction(self.set_trans_color, transpanel, wx.NullColour))
+        transpanel.SetTransparent(int)
+        sizer.Add(transpanel)
         panel.SetSizer(sizer)
         panel.Bind(wx.EVT_LEFT_DCLICK, RunFunction(self.run_file, path))
         panel.Bind(wx.EVT_RIGHT_UP, RunFunction(self.show_menu, path))
-        panel.Bind(wx.EVT_ENTER_WINDOW, RunFunction(panel.SetBackgroundColour, "#444444"))
-        panel.Bind(wx.EVT_LEAVE_WINDOW, RunFunction(panel.SetBackgroundColour, wx.NullColour))
         self.psizer.Add(panel, proportion=1)
+
+    def set_trans_color(self, panel, color):
+        panel.SetBackgroundColour(color)
+        if color == wx.NullColour:
+            panel.SetTransparent(int)
+        else:
+            panel.SetTransparent(200)
 
     def show_menu(self, path, directory=False):
         menu = wx.Menu()
